2be781974a6c804878618be3824df64d1c259110
Mykhel <mikkybrown300@gmail.com>
2023-06-21 16:46:31 +0100

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,13 +4,15 @@
     Define 'add_integer' function.
 """
 
-
 def add_integer(a, b=98):
+    """Adds two numbers
+    Args:
+        a - first number input
+        b - second number input
+
     """
-        Return: Sum of integers a & b.
-    """
-    if ((not isinstance(a, int) and not isinstance(a, float))):
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    return int(a) + int(b)
